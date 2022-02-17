c0a084319c0394105c3a7a5688135e48f7cf5898
lebrony <asandler778@gmail.com>
2022-02-18 02:13:06 +0300

    table layout for buttons

---
+++ app/src/main/java/com/techspeck/tictactoe/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.techspeck.tictactoe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    protected fun btnClick(view:View){
+        val btnSelected = view as Button
+    }
+}
