8f51287aac9ba95a45ca77a870b82a71f72b60af
lebrony <asandler778@gmail.com>
2022-02-18 02:19:11 +0300

    linking buttons id to main activity

--- app/src/main/java/com/techspeck/tictactoe/MainActivity.kt
+++ app/src/main/java/com/techspeck/tictactoe/MainActivity.kt
@@ -13,5 +13,17 @@ class MainActivity : AppCompatActivity() {
 
     protected fun btnClick(view:View){
         val btnSelected = view as Button
+        var CellID = 0
+        when (btnSelected.id){
+            R.id.bttn1 -> CellID=1
+            R.id.bttn2 -> CellID = 2
+            R.id.bttn3 -> CellID = 3
+            R.id.bttn4 -> CellID = 4
+            R.id.bttn5 -> CellID = 5
+            R.id.bttn6 -> CellID = 6
+            R.id.bttn7 -> CellID = 7
+            R.id.bttn8 -> CellID = 8
+            R.id.bttn9 -> CellID = 9
+        }
     }
 }
